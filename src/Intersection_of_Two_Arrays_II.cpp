80ab9dc71509d8b89816caaf5db527e74639e47e
EverestMountain <1574293921@qq.com>
2021-09-17 21:38:07 +0800

    数组的交集

---
+++ src/Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,39 @@
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
+        unordered_map<int, int> mp1;
+        unordered_map<int, int> mp2;
+        vector<int> res;
+        for (auto &x: nums1)
+            mp1[x] += 1;
+        for (auto &x: nums2)
+            mp2[x] += 1;
+        for (auto x: mp1) {
+            if (mp2.find(x.first) != mp2.end()) {
+                int n1 = mp1[x.first];
+                int n2 = mp2[x.first];
+                for (int i = 0; i < min(n1, n2); i++) {
+                    res.push_back(x.first);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    vector<tuple<vector<int>, vector<int>, vector<int>>> test_cases;
+    test_cases.emplace_back(make_tuple<vector<int>, vector<int>, vector<int>>({1, 2, 2, 1}, {2, 2}, {2, 2}));
+    test_cases.emplace_back(make_tuple<vector<int>, vector<int>, vector<int>>({4, 9, 5}, {9, 4, 9, 8, 4}, {9, 4}));
+    Solution sol;
+    for (auto &test_case: test_cases) {
+        assert(sol.intersect(std::get<0>(test_case), std::get<1>(test_case)) == std::get<2>(test_case));
+    }
+    return 0;
+}
